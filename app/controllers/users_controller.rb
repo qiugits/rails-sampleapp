7c1e212f94e29ef8620f3ce4f1dda5fedb223bf0
Qiu <qiu.gits@gmail.com>
2018-03-04 22:58:17 +0900

    Finish 10.2.1 restrict so can update only when logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # Only when :edit, :update, Execute :logged_in_user, which require user to be logged in.
+  before_action :logged_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -37,5 +39,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
     end
 end
