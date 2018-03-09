caeb3544e28a5f85d4ed797103f4c18bbb81e504
Qiu <qiu.gits@gmail.com>
2018-03-09 19:31:37 +0900

    Finish 10.3.1 Add users index. Test header links for both when logged in or not

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,5 +15,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "header links when not logged in" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "header links when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    #assert_select "a[href=?]", user_url(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
