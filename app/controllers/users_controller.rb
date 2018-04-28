68e990cca580d5ea5547398ea282bfa4bb9e7b3f
Qiu <qiu.gits@gmail.com>
2018-04-29 04:01:46 +0900

    Add user delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   # Only when :edit, :update, Execute :logged_in_user, which require user to be logged in.
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])  # :pageはwill_paginateが自動生成
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -60,5 +67,9 @@ class UsersController < ApplicationController
         flash[:danger] = 'Action not permitted'
         redirect_to root_url
       end
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
