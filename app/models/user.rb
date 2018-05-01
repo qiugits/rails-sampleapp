6e0605687f26f51ddc1c0416d97922d075c72454
Qiu <qiu.gits@gmail.com>
2018-05-01 21:57:58 +0900

    Finish 11.1.2 Add create_activation_digest method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -37,5 +38,17 @@ class User < ApplicationRecord
   # throw away user log in info
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+  # Downcase all letters of email
+  def downcase_email
+    self.email.downcase!
+  end
+
+  # Create and assign activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
