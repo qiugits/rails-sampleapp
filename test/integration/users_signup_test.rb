7c4ae2cbe34143dcad9a3ddd65b1f8d051807cf2
Qiu <qiu.gits@gmail.com>
2018-05-09 04:35:00 +0900

    Finish 11.2.4 update user create action

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,10 +7,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference "User.count" do
-      post signup_path, params: { user: { name: '',
-                                         email: 'user@invalid',
-                                         password: 'foo',
-                                         password_confirmation: 'bar' } }
+      post signup_path, params: { user: { 
+        name: '',
+        email: 'user@invalid',
+        password: 'foo',
+        password_confirmation: 'bar' 
+      } }
     end
     assert_template 'users/new'
     # assert_select 'form[action="/signup"]'
@@ -22,13 +24,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: 'Example User',
-                                         email: 'user@valid.com',
-                                         password: 'strongPass',
-                                         password_confirmation: 'strongPass' } }
+      post users_path, params: { user: { 
+        name: 'Example User',
+        email: 'user@valid.com',
+        password: 'strongPass',
+        password_confirmation: 'strongPass'
+      } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
