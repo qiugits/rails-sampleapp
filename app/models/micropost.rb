da2b411b7c3a4d4759ae15f0d9de915fc674632a
Qiu <qiu.gits@gmail.com>
2018-05-18 21:56:34 +0900

    Finish 13.4.1 add picture uploader and its test

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   # `-> { puts "foo" }`はラムダ式（Procとも）`.call`で実行する
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
