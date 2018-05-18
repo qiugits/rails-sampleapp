2bd17834317f1aaf6eef2ab634fa3b7733e87bfc
Qiu <qiu.gits@gmail.com>
2018-05-18 17:13:48 +0900

    Finish 13.3.3 add micropost feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,11 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+  # feed prototype
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   
   private
   # Downcase all letters of email
