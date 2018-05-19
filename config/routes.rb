7d182236afd27d76a7ec312c30ba6e9c2a3cd22f
Qiu <qiu.gits@gmail.com>
2018-05-20 00:54:06 +0900

    Finish 14.2.2 add stats and follow/unfollow hidden form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   # Users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  # Relationships
+  resources :relationships, only: [:create, :destroy]
   # Micropost
   resources :microposts, only: [:create, :destroy]
   # Account Activations
