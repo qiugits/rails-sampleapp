03a48bb5148089c4b3df2cdc4aa61e624f79c130
Qiu <qiu.gits@gmail.com>
2018-05-20 14:47:51 +0900

    Finish 14.2.5 use Ajax: render js instead of html when follow/unfollowing

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Sampleapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Embed authentication token to remote form
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
